fc4a1ad47808d6bd77ae0d7f998f9b618647907b
JohnAnthony795 <johnanthony795@laposte.net>
2017-01-13 14:38:39 +0100

    Cleanup et relecture

--- main.c
+++ main.c
@@ -7,30 +7,30 @@
  * \brief Initialisation des structures de l'application (tâches, mutex, 
  * semaphore, etc.)
  */
-void initStruct(void);
+void initStruct (void);
 
 /**
  * \fn void startTasks(void)
  * \brief Démarrage des tâches
  */
-void startTasks(void);
+void startTasks (void);
 
 /**
  * \fn void deleteTasks(void)
  * \brief Arrêt des tâches
  */
-void deleteTasks(void);
+void deleteTasks (void);
 
-int main(int argc, char**argv) {
-    printf("#################################\n");
-    printf("#      DE STIJL PROJECT         #\n");
-    printf("#################################\n");
+int main (int argc, char **argv) {
+    printf ("#################################\n");
+    printf ("#      DE STIJL PROJECT         #\n");
+    printf ("#################################\n");
 
     //signal(SIGTERM, catch_signal);
     //signal(SIGINT, catch_signal);
 
     /* Avoids memory swapping for this program */
-    mlockall(MCL_CURRENT | MCL_FUTURE);
+    mlockall (MCL_CURRENT | MCL_FUTURE);
     /* For printing, please use rt_print_auto_init() and rt_printf () in rtdk.h
      * (The Real-Time printing library). rt_printf() is the same as printf()
      * except that it does not leave the primary mode, meaning that it is a
@@ -41,139 +41,139 @@ int main(int argc, char**argv) {
      * before any calls to rt_printf(). If you forget this part, you won't see
      * anything printed.
      */
-    rt_print_auto_init(1);
-    initStruct();
-    startTasks();
-    pause();
-    deleteTasks();
+    rt_print_auto_init (1);
+    initStruct ();
+    startTasks ();
+    pause ();
+    deleteTasks ();
 
     return 0;
 }
 
-void initStruct(void) {
+void initStruct (void) {
     int err;
     /* Creation des mutex */
-    if (err = rt_mutex_create(&mutexEtat, NULL)) {
-        rt_printf("Error mutex create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_mutex_create (&mutexEtat, NULL)) {
+        rt_printf ("Error mutex create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
-    if (err = rt_mutex_create(&mutexMove, NULL)) {
-        rt_printf("Error mutex create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_mutex_create (&mutexMove, NULL)) {
+        rt_printf ("Error mutex create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
-    if (err = rt_mutex_create(&mutexModeCalcul, NULL)) {
-        rt_printf("Error mutex create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_mutex_create (&mutexModeCalcul, NULL)) {
+        rt_printf ("Error mutex create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
-    if (err = rt_mutex_create(&mutexEtatCamera, NULL)) {
-        rt_printf("Error mutex create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
-    }    
-   if (err = rt_mutex_create(&mutexCompteur, NULL)) {
-        rt_printf("Error mutex create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_mutex_create (&mutexEtatCamera, NULL)) {
+        rt_printf ("Error mutex create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
-  if (err = rt_mutex_create(&mutexComm, NULL)) {
-        rt_printf("Error mutex create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_mutex_create (&mutexCompteur, NULL)) {
+        rt_printf ("Error mutex create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
+    }
+    if (err = rt_mutex_create (&mutexComm, NULL)) {
+        rt_printf ("Error mutex create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
 
     /* Creation du semaphore */
-    if (err = rt_sem_create(&semConnecterRobot, NULL, 0, S_FIFO)) {
-        rt_printf("Error semaphore create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_sem_create (&semConnecterRobot, NULL, 0, S_FIFO)) {
+        rt_printf ("Error semaphore create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
-if (err = rt_sem_create(&semWatchdog, NULL, 1, S_FIFO)) {
-        rt_printf("Error semaphore create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_sem_create (&semWatchdog, NULL, 1, S_FIFO)) {
+        rt_printf ("Error semaphore create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
-if (err = rt_sem_create(&semBatterie, NULL, 1, S_FIFO)) {
-        rt_printf("Error semaphore create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_sem_create (&semBatterie, NULL, 1, S_FIFO)) {
+        rt_printf ("Error semaphore create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
 
     /* Creation des taches */
-    if (err = rt_task_create(&tServeur, NULL, 0, PRIORITY_TSERVEUR, 0)) {
-        rt_printf("Error task create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_task_create (&tServeur, NULL, 0, PRIORITY_TSERVEUR, 0)) {
+        rt_printf ("Error task create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
-    if (err = rt_task_create(&tconnect, NULL, 0, PRIORITY_TCONNECT, 0)) {
-        rt_printf("Error task create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_task_create (&tconnect, NULL, 0, PRIORITY_TCONNECT, 0)) {
+        rt_printf ("Error task create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
-    if (err = rt_task_create(&tmove, NULL, 0, PRIORITY_TMOVE, 0)) {
-        rt_printf("Error task create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_task_create (&tmove, NULL, 0, PRIORITY_TMOVE, 0)) {
+        rt_printf ("Error task create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
-    if (err = rt_task_create(&tenvoyer, NULL, 0, PRIORITY_TENVOYER, 0)) {
-        rt_printf("Error task create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_task_create (&tenvoyer, NULL, 0, PRIORITY_TENVOYER, 0)) {
+        rt_printf ("Error task create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
-    if (err = rt_task_create(&tcalcul, NULL, 0, PRIORITY_TCALCUL, 0)) {
-        rt_printf("Error task create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
-    }
-
- if (err = rt_task_create(&tbat, NULL, 0, PRIORITY_TBAT, 0)) {
-        rt_printf("Error task create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_task_create (&tcalcul, NULL, 0, PRIORITY_TCALCUL, 0)) {
+        rt_printf ("Error task create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
 
- if (err = rt_task_create(&twatch, NULL, 0, PRIORITY_TWATCH, 0)) {
-        rt_printf("Error task create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_task_create (&tbat, NULL, 0, PRIORITY_TBAT, 0)) {
+        rt_printf ("Error task create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
+    }
+
+    if (err = rt_task_create (&twatch, NULL, 0, PRIORITY_TWATCH, 0)) {
+        rt_printf ("Error task create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
 
     /* Creation des files de messages */
-    if (err = rt_queue_create(&queueMsgGUI, "toto", MSG_QUEUE_SIZE*sizeof(DMessage), MSG_QUEUE_SIZE, Q_FIFO)){
-        rt_printf("Error msg queue create: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_queue_create (&queueMsgGUI, "toto", MSG_QUEUE_SIZE * sizeof (DMessage), MSG_QUEUE_SIZE, Q_FIFO)) {
+        rt_printf ("Error msg queue create: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
 
     /* Creation des structures globales du projet */
-    robot = d_new_robot();
-    move = d_new_movement();
-    serveur = d_new_server();
-    camera = d_new_camera();
-    arena = d_new_arena();
+    robot = d_new_robot ();
+    move = d_new_movement ();
+    serveur = d_new_server ();
+    camera = d_new_camera ();
+    arena = d_new_arena ();
 }
 
-void startTasks() {
+void startTasks () {
     int err;
-    if (err = rt_task_start(&tconnect, &connecter, NULL)) {
-        rt_printf("Error task start: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_task_start (&tconnect, &connecter, NULL)) {
+        rt_printf ("Error task start: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
-    if (err = rt_task_start(&tServeur, &communiquer, NULL)) {
-        rt_printf("Error task start: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_task_start (&tServeur, &communiquer, NULL)) {
+        rt_printf ("Error task start: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
-    if (err = rt_task_start(&tmove, &deplacer, NULL)) {
-        rt_printf("Error task start: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_task_start (&tmove, &deplacer, NULL)) {
+        rt_printf ("Error task start: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
-    if (err = rt_task_start(&tenvoyer, &envoyer, NULL)) {
-        rt_printf("Error task start: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_task_start (&tenvoyer, &envoyer, NULL)) {
+        rt_printf ("Error task start: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
-    if (err = rt_task_start(&tcalcul, &calcul_position, NULL)) {
-        rt_printf("Error task start: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_task_start (&tcalcul, &calcul_position, NULL)) {
+        rt_printf ("Error task start: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
-    if (err = rt_task_start(&tbat, &batterie, NULL)) {
-        rt_printf("Error task start: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_task_start (&tbat, &batterie, NULL)) {
+        rt_printf ("Error task start: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
-  if (err = rt_task_start(&twatch, &watchdog, NULL)) {
-        rt_printf("Error task start: %s\n", strerror(-err));
-        exit(EXIT_FAILURE);
+    if (err = rt_task_start (&twatch, &watchdog, NULL)) {
+        rt_printf ("Error task start: %s\n", strerror (-err));
+        exit (EXIT_FAILURE);
     }
 }
 
-void deleteTasks() {
-    rt_task_delete(&tServeur);
-    rt_task_delete(&tconnect);
-    rt_task_delete(&tmove);
-rt_task_delete(&tbat);
-rt_task_delete(&twatch);
+void deleteTasks () {
+    rt_task_delete (&tServeur);
+    rt_task_delete (&tconnect);
+    rt_task_delete (&tmove);
+    rt_task_delete (&tbat);
+    rt_task_delete (&twatch);
 }
